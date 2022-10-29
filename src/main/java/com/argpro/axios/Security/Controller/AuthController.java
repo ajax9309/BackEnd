744e251383e13aa729be8c81ed2b4bdf17c09cd3
ajax9309 <alexzomboe@gmail.com>
2022-10-29 17:13:14 -0300

    authCORS

--- src/main/java/com/argpro/axios/Security/Controller/AuthController.java
+++ src/main/java/com/argpro/axios/Security/Controller/AuthController.java
@@ -38,7 +38,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200","https://axiostestap.web.app"})
+
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
